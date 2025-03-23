b7e5c4b5953a8f0e30db2e4b7f4f8d04c4163204
Mayoroftata <objmak@gmail.com>
2025-03-23 22:41:40 +0100

    replace button with link for navigation on booking successful page

--- src/app/bookingsuccessful/page.js
+++ src/app/bookingsuccessful/page.js
@@ -12,7 +12,7 @@ const Page = () => {
                     <p className='text-center text-xl font-bold text-white'>Booking Successful</p>
                     <p className='text-center px-4 text-xl font-bold text-white'>Thank you for choosing Deeluck hotel, your receipt have been sent to you email</p>
                     <div className='text-center'>
-                        <button className='bg-blue-900 text-white font-base rounded border border-white px-5 py-2 mt-3'>Back Home</button>
+                        <Link href={"/"} className='bg-blue-900 text-white font-base rounded border border-white px-5 py-2 mt-3'>Back Home</Link>
                     </div>
                 </div>
             </div>
